656e963b8dd0e2b1b3b71eeedbd95bc7cd1f40b1
Munkh-AmgalanDav <haay.maay@gmail.com>
2024-08-21 12:24:09 -0400

    semantic html & css grid

---
+++ indext.js
@@ -0,0 +1,25 @@
+// const num = [1, 2, 3, 4, 5]
+// let i = 0;
+// let sum = 0;
+
+// while(i<num.length){
+
+//     sum = sum + num[i];
+
+//     i++;
+
+// }
+// console.log(sum);
+
+
+
+const array = [1, 2, 3, 4, 5]
+let newArray = [];
+
+for (let i = array.length - 1; i >= 0; i--){
+    // console.log(array[i]);
+    newArray += array[i];
+
+}
+
+console.log(newArray);
